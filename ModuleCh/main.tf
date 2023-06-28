9635cab0308e9cf9e1e6a09233357b779962a419
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-28 15:21:30 +0200

    Update main.tf

--- ModuleCh/main.tf
+++ ModuleCh/main.tf
@@ -5,6 +5,7 @@ resource "azurerm_resource_group" "kubeadm" {
 
 
 resource "azurerm_public_ip" "kubeadm_public_ip" {
+   count = 3
    name = "kubeadm_public_ip${count.index}"
    location = var.location
    resource_group_name = azurerm_resource_group.kubeadm.name
