145eb885d26571ac9a3623f7a0895c291bdb94dd
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-28 15:15:44 +0200

    Update outputs.tf

--- ModuleCh/outputs.tf
+++ ModuleCh/outputs.tf
@@ -1,19 +1,3 @@
-output "Ressource_group_name" {
-  value = azurerm_resource_group.kubeadm.name
-}
-
-output "Environment" {
-  value = var.environment
-}
-
-output "The_subnet_ID" {
- value = azurerm_subnet.kubeadm-subnet.id
-}
-
-output "The_vnet_ID" {
- value = azurerm_virtual_network.kubeadm-net.id
-}
-
 output "The_kubeadm_Private_ip" {
    value = azurerm_linux_virtual_machine.kubeadm.private_ip_address
 }
